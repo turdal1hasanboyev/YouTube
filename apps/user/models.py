d51381130161b6c0cc76bb0f76764cb441385a2e
turdal1hasanboyev <turdalihasanboyev70.com>
2024-09-07 20:55:11 +0500

    Written user app models!

--- apps/user/models.py
+++ apps/user/models.py
@@ -9,7 +9,40 @@ from apps.common.models import BaseModel
 
 class User(BaseModel, AbstractUser):
     """User model"""
-    bio = RichTextField(null=True, blank=True)
+
+    GENDER = (
+        ('male', ('Male')),
+        ('female', ('Female')),
+    )
+
+    OCCUPATION = (
+        ('student', ('Student')),
+        ('worker', ('Worker')),
+    )
+
+    FAVOURITE_SOCIAL_NETWORK = (
+        ('telegram', ('Telegram')),
+        ('instagram', ('Instagram')),
+        ('youtube', ('YouTube')),
+    )
+
+    description = RichTextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    phone_number = models.CharField(max_length=225, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER, max_length=225, null=True, blank=True)
+    occupation = models.CharField(choices=OCCUPATION, max_length=225, null=True, blank=True)
+    adress = models.CharField(max_length=225, null=True, blank=True)
+    followers_count = models.IntegerField(default=0, null=True, blank=True)
+    following_count = models.IntegerField(default=0, null=True, blank=True)
+    last_active = models.DateField(null=True, blank=True)
+    favourite_social_network = models.CharField(choices=FAVOURITE_SOCIAL_NETWORK, max_length=225, null=True, blank=True)
 
     def __str__(self):
-        return self.username
+        if self.get_full_name():
+            return f"{self.id} - {self.get_full_name()}"
+        
+        elif self.email:
+            return f"{self.id} - {self.email}"
+        
+        return f"{self.id} - {self.username}"
